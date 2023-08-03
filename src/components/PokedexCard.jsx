4c9b4aa6b92be446e5c621c06d700fb0f92045a7
Sebastian-Simek <sebastiansimek09@gmail.com>
2023-08-03 10:55:36 -0700

    ft. refactored pokemon state to remove placeholder object

--- src/components/PokedexCard.jsx
+++ src/components/PokedexCard.jsx
@@ -1,33 +1,27 @@
 import PropTypes from "prop-types";
 
 export default function PokedexCard({ pokemon }) {
-  function switchClasses(id) {
-    const ashClassName = "ash";
-    const pokedexCardClassName = "pokedex-card-container";
-    if (id == 152) {
-      return ashClassName;
-    } else {
-      return pokedexCardClassName;
-    }
-  }
-
   return (
-    <div className={pokemon.id === 152 ? "ash" : "pokedex-card-container"}>
-      <div className="pokedex-card">
-        {pokemon.id != 152 && `#${pokemon.id}`}
-        <b>{pokemon.name}</b>
-        <img src={pokemon.gif} />
-      </div>
-      <div className="pokedex-details">
-        {pokemon.type1 && (
-          <em>
-            Type: {pokemon.type1}
-            {pokemon.type2 && `/${pokemon.type2}`}
-          </em>
-        )}
-        {pokemon.description && <p>{pokemon.description}</p>}
-      </div>
-    </div>
+    <>
+      {pokemon.id && (
+        <div className="pokedex-card-container">
+          <div className="pokedex-card">
+            {pokemon.id && `#${pokemon.id}`}
+            <b>{pokemon.name}</b>
+            <img src={pokemon.gif} />
+          </div>
+          <div className="pokedex-details">
+            {pokemon.type1 && (
+              <em>
+                Type: {pokemon.type1}
+                {pokemon.type2 && `/${pokemon.type2}`}
+              </em>
+            )}
+            {pokemon.description && <p>{pokemon.description}</p>}
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 PokedexCard.propTypes = {
